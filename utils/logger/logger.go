0f4120769a17f6d9c5dc647bdb3fe673b78270da
NewtonZhan <zhanyunze0601@gmail.com>
2026-06-25 11:14:39 +0800

    feat(logger): Add testing cases for logger

--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -2,19 +2,23 @@ package logger
 
 import (
 	"fmt"
+	"testing"
 )
 
 // Logger datastructure create logs for each service
 type Logger struct {
 	loggerName string
+	testLogger **testing.T
 }
 
 // NewLogger creates a logger
 func NewLogger(
 	loggerName string,
+	testLogger **testing.T, // testLogger have to pass in testing.T
 ) *Logger {
 	return &Logger{
 		loggerName: loggerName,
+		testLogger: testLogger,
 	}
 }
 
@@ -22,24 +26,40 @@ func NewLogger(
 func (logger *Logger) Info(
 	message string,
 ) {
-	syslogger.Info(
-		fmt.Sprintf(
-			"%s: %s",
+	if syslogger != nil {
+		syslogger.Info(
+			fmt.Sprintf(
+				"%s: %s",
+				logger.loggerName,
+				message,
+			),
+		)
+	} else if logger.testLogger != nil {
+		(*logger.testLogger).Logf(
+			"[INFO]%s: %s",
 			logger.loggerName,
 			message,
-		),
-	)
+		)
+	}
 }
 
 // Error logs an error message
 func (logger *Logger) Error(
 	message string,
 ) {
-	syslogger.Error(
-		fmt.Sprintf(
-			"%s: %s",
+	if syslogger != nil {
+		syslogger.Error(
+			fmt.Sprintf(
+				"%s: %s",
+				logger.loggerName,
+				message,
+			),
+		)
+	} else if logger.testLogger != nil {
+		(*logger.testLogger).Logf(
+			"[ERROR]%s: %s",
 			logger.loggerName,
 			message,
-		),
-	)
+		)
+	}
 }
